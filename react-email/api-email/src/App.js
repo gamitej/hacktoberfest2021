ced06195abec86f1d34a1d1c221298b5382a0da1
amisha26 <amisha8299@gmail.com>
2021-10-08 21:52:35 +0530

    apponity project error solved

---
+++ react-email/api-email/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Send from "./comp/Send";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Send />
+      </div>
+    </div>
+  );
+}
+
+export default App;
